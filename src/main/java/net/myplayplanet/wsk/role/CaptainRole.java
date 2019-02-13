7868e446fc4f3cb13c0b9f8a140d8ecb41a8f621
Butzlabben <darkstylerz@web.de>
2019-02-13 21:54:44 +0100

    Implemented GunnerRole

--- src/main/java/net/myplayplanet/wsk/role/CaptainRole.java
+++ src/main/java/net/myplayplanet/wsk/role/CaptainRole.java
@@ -1,8 +1,65 @@
 package net.myplayplanet.wsk.role;
 
+import org.bukkit.Color;
+import org.bukkit.Material;
+import org.bukkit.enchantments.Enchantment;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.PotionMeta;
+import org.bukkit.potion.PotionEffect;
+import org.bukkit.potion.PotionEffectType;
+
 public class CaptainRole extends AbstractRole {
+
     @Override
     protected void setInventory() {
+        ItemStack mainWeapon = new ItemStack(Material.GOLDEN_SWORD);
+        mainWeapon.addEnchantment(Enchantment.DAMAGE_ALL, 1);
 
+        addItem(0, mainWeapon);
+        addItem(1, bow);
+        addItem(2, iron_pickaxe);
+        ItemStack is = new ItemStack(Material.POTION);
+        PotionMeta pm = (PotionMeta) is.getItemMeta();
+        pm.setDisplayName("§9Unterwasseratmung");
+        pm.setColor(Color.BLUE);
+        PotionEffect pe = new PotionEffect(PotionEffectType.WATER_BREATHING, 3600, 0);
+        pm.addCustomEffect(pe, true);
+        is.setItemMeta(pm);
+        addItem(3, is);
+        addItem(4, tnt);
+        addItem(5, tnt);
+        addItem(6, tnt);
+        addItem(7, concrete);
+        addItem(8, concrete);
+        // Obere Reihe
+        addItem(9, tnt);
+        addItem(10, tnt);
+        addItem(11, tnt);
+        addItem(12, tnt);
+        addItem(13, tnt);
+        addItem(14, tnt);
+        addItem(15, tnt);
+        addItem(16, tnt);
+        addItem(17, arrows);
+        // Mittlere Reihe
+        addItem(18, slime);
+        addItem(19, stickyPiston);
+        addItem(20, piston);
+        addItem(21, tnt);
+        addItem(22, tnt);
+        addItem(23, tnt);
+        addItem(24, tnt);
+        addItem(25, tnt);
+        addItem(26, tnt);
+        // Unterste Reihe
+        addItem(27, observer);
+        addItem(28, redstoneBlock);
+        addItem(29, redstoneTorch);
+        addItem(30, redstoneComparator);
+        addItem(31, redstoneRepeater);
+        addItem(32, redstone);
+        addItem(33, lever);
+        addItem(34, wooden_plate);
+        addItem(35, wooden_button);
     }
 }
--- src/main/java/net/myplayplanet/wsk/role/GunnerRole.java
+++ src/main/java/net/myplayplanet/wsk/role/GunnerRole.java
@@ -1,8 +1,49 @@
 package net.myplayplanet.wsk.role;
 
+import org.bukkit.Material;
+import org.bukkit.inventory.ItemStack;
+
 public class GunnerRole extends AbstractRole {
+    
     @Override
     protected void setInventory() {
+        addItem(0, new ItemStack(Material.WOODEN_SWORD));
+        addItem(1, bow);
+        addItem(2, arrows);
+        addItem(3, wooden_pickaxe);
+        addItem(4, concrete);
+        addItem(5, concrete);
+        addItem(6, concrete);
+        addItem(7, tnt);
+        addItem(8, tnt);
+        addItem(9, tnt);
+        addItem(10, tnt);
+        addItem(11, tnt);
+        addItem(12, tnt);
+        addItem(13, tnt);
+        addItem(14, tnt);
+        addItem(15, tnt);
+        addItem(16, tnt);
+        addItem(17, tnt);
+        addItem(18, tnt);
+        addItem(19, tnt);
+        addItem(20, tnt);
+        addItem(21, tnt);
+        addItem(22, tnt);
+        addItem(23, tnt);
+        addItem(24, tnt);
+        addItem(25, tnt);
+        addItem(26, tnt);
+        addItem(27, tnt);
+        addItem(28, tnt);
+        addItem(29, tnt);
+        addItem(30, tnt);
+        addItem(31, tnt);
+        addItem(32, tnt);
+        addItem(33, tnt);
+        addItem(34, tnt);
+        addItem(35, tnt);
 
+        addItem(38, new ItemStack(Material.CHAINMAIL_CHESTPLATE));
     }
 }
